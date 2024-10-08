b5b461b0a200d15be8bf58090e624b2a3fbdc965
Kitsunex-glitch <fairDarkworker@gmail.com>
2024-10-08 13:40:34 +0300

    Fixes

---
+++ VK UI3/StaticParams.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VK_UI3
+{
+    internal class StaticParams
+    {
+
+        public static readonly string tokenStatDefaultGate = Environment.GetEnvironmentVariable("TOKEN_STAT_DEFAULT_GATE");
+    }
+}
