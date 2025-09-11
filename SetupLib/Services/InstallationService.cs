a21b7a6dc043102c3f9f2a03ca48f28e7023055f
Kitsunex-glitch <fairDarkworker@gmail.com>
2025-09-11 17:19:03 +0300

    fix updater zip

--- SetupLib/Services/InstallationService.cs
+++ SetupLib/Services/InstallationService.cs
@@ -139,9 +139,19 @@ namespace SetupLib.Services
             }
             else // PackageType.ZIP
             {
-                // Определяем путь к Program Files на системном диске
-                string programFilesPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
-                string extractPath = Path.Combine(programFilesPath, "Music M");
+                string extractPath;
+                if (PathInstallZIP == null)
+                {
+                    // Определяем путь к Program Files на системном диске
+                    string programFilesPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                    extractPath = Path.Combine(programFilesPath, "Music M");
+
+                }
+                else
+                                    {
+                    // Используем указанный путь
+                    extractPath = PathInstallZIP;
+                }
                 command = GetZipInstallCommand(destinationPath, extractPath);
             }
 
